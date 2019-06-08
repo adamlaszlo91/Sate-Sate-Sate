975ee9dedf034aa0c3028561b301237ea55d058a
Ádám László <adam.laszlo.91@gmail.com>
2019-06-08 17:15:16 +0200

    Added the possibility of Meliodas clones with random placement
    new: Added ability to add Meliodas programatically
    new: Clones can be created and added to the view with random snapping to one of the parent's sides
    new: Added a placeholder for the images

--- app/src/main/java/hu/evehcilabs/satesatesate/fragment/MainFragment.java
+++ app/src/main/java/hu/evehcilabs/satesatesate/fragment/MainFragment.java
@@ -1,18 +1,24 @@
 package hu.evehcilabs.satesatesate.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.ViewCompat;
 import hu.evehcilabs.androidbase.BaseFragment;
 import hu.evehcilabs.satesatesate.R;
 import hu.evehcilabs.satesatesate.view.MeliodasImageView;
 
 public class MainFragment extends BaseFragment {
+  private static final int LOSTVAYNE_CLONE_MULTIPLIER = 4;
 
+  private ConstraintLayout meliodasFigureContainer;
   private MeliodasImageView meliodasImageView;
+  private MeliodasImageView[] meliodasClones;
 
   public static MainFragment newInstance() {
     return new MainFragment();
@@ -29,16 +35,41 @@ public class MainFragment extends BaseFragment {
   }
 
   private void initViews(View view) {
+    meliodasFigureContainer = view.findViewById(R.id.container_meliodas_figure);
     meliodasImageView = view.findViewById(R.id.image_meliodas);
   }
 
   private void initActions() {
     meliodasImageView.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
-        meliodasImageView.getAnimationHelper().reStartWiggleAnimation();
+        //meliodasImageView.getAnimationHelper().reStartWiggleAnimation();
+        makeMeliodasClonesAppear();
       }
     });
   }
+
+  // region Meliodas clones
+
+  private void makeMeliodasClonesAppear() {
+    initMeliodasClones();
+  }
+
+  private void initMeliodasClones() {
+    meliodasClones = new MeliodasImageView[LOSTVAYNE_CLONE_MULTIPLIER];
+    for (int i = 0; i < LOSTVAYNE_CLONE_MULTIPLIER; i++) {
+      MeliodasImageView meliodasClone = new MeliodasImageView(getActivity());
+      meliodasClone.setId(ViewCompat.generateViewId());
+      meliodasFigureContainer.addView(meliodasClone);
+      meliodasClone.setupSizeAndRandomPosition();
+      meliodasClones[i] = meliodasClone;
+    }
+  }
+
+  private void startMeliodasClonesAnimation() {
+
+  }
+
+  // endregion
 
   // region Animation
 
--- app/src/main/java/hu/evehcilabs/satesatesate/view/MeliodasImageView.java
+++ app/src/main/java/hu/evehcilabs/satesatesate/view/MeliodasImageView.java
@@ -2,22 +2,39 @@ package hu.evehcilabs.satesatesate.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import hu.evehcilabs.satesatesate.R;
 import hu.evehcilabs.satesatesate.helper.AnimationHelper;
+import java.util.Random;
 
 public class MeliodasImageView extends androidx.appcompat.widget.AppCompatImageView {
 
+  enum PositionInParent {
+    TOP, BOTTOM, LEFT, RIGHT, CENTER;
+  }
+
   private AnimationHelper animationHelper;
+  private PositionInParent positionInParent = PositionInParent.CENTER;
 
   public MeliodasImageView(Context context) {
     super(context);
+    customInit();
   }
 
   public MeliodasImageView(Context context, AttributeSet attrs) {
     super(context, attrs);
+    customInit();
   }
 
   public MeliodasImageView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+    customInit();
+  }
+
+  private void customInit() {
+    setImageResource(R.drawable.placeholder_common);
+    setScaleType(ScaleType.CENTER_INSIDE);
   }
 
   public AnimationHelper getAnimationHelper() {
@@ -33,6 +50,67 @@ public class MeliodasImageView extends androidx.appcompat.widget.AppCompatImageV
     animationHelper.destroy();
     super.onDetachedFromWindow();
   }
+
+  // region Layout parameter generation
+
+  public void setupSizeAndRandomPosition() {
+    setupLayoutParams();
+    setupConstraintsForSizeAndCenterPosition();
+    setupConstraintsForRandomPositionAndPerpendicularBias();
+  }
+
+  public void setupSizeAndCenterPosition() {
+    setupLayoutParams();
+    setupConstraintsForSizeAndCenterPosition();
+  }
+
+  private void setupLayoutParams() {
+    ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(0, 0);
+    setLayoutParams(layoutParams);
+  }
+
+  private void setupConstraintsForSizeAndCenterPosition() {
+    ConstraintLayout parent = (ConstraintLayout)getParent();
+    ConstraintSet constraintSet = new ConstraintSet();
+    constraintSet.clone(parent);
+    constraintSet.constrainPercentWidth(getId(), 0.5f);
+    constraintSet.setDimensionRatio(getId(), "1:1");
+    constraintSet.connect(getId(), ConstraintSet.RIGHT, parent.getId(), ConstraintSet.RIGHT, 0);
+    constraintSet.connect(getId(), ConstraintSet.LEFT, parent.getId(), ConstraintSet.LEFT, 0);
+    constraintSet.connect(getId(), ConstraintSet.BOTTOM, parent.getId(), ConstraintSet.BOTTOM, 0);
+    constraintSet.connect(getId(), ConstraintSet.TOP, parent.getId(), ConstraintSet.TOP, 0);
+    constraintSet.applyTo(parent);
+  }
+
+  private void setupConstraintsForRandomPositionAndPerpendicularBias() {
+    ConstraintLayout parent = (ConstraintLayout)getParent();
+    ConstraintSet constraintSet = new ConstraintSet();
+    constraintSet.clone(parent);
+    PositionInParent randomPositionExcludingCenter =
+      PositionInParent.values()[new Random().nextInt(PositionInParent.values().length-1)];
+    switch (randomPositionExcludingCenter) {
+      case TOP:
+        constraintSet.setVerticalBias(getId(), 0.0f);
+        constraintSet.setHorizontalBias(getId(), new Random().nextFloat());
+        break;
+      case LEFT:
+        constraintSet.setHorizontalBias(getId(), 0.0f);
+        constraintSet.setVerticalBias(getId(), new Random().nextFloat());
+        break;
+      case BOTTOM:
+        constraintSet.setVerticalBias(getId(), 1.0f);
+        constraintSet.setHorizontalBias(getId(), new Random().nextFloat());
+        break;
+      case RIGHT:
+        constraintSet.setHorizontalBias(getId(), 1.0f);
+        constraintSet.setVerticalBias(getId(), new Random().nextFloat());
+      default:
+    }
+    positionInParent = randomPositionExcludingCenter;
+    constraintSet.applyTo(parent);
+  }
+
+  // endregion
 
   // region Animations
 
