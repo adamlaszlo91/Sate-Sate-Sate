fa1c3e6d2f68c4df3be07970f6f39543d9228b08
Ádám László <adam.laszlo.91@gmail.com>
2019-08-30 21:27:00 +0200

    Updated to use DataBinding
    The project now uses data bindig to improve my skills.

--- app/src/main/java/hu/evehcilabs/satesatesate/activity/MainActivity.java
+++ app/src/main/java/hu/evehcilabs/satesatesate/activity/MainActivity.java
@@ -1,35 +1,31 @@
 package hu.evehcilabs.satesatesate.activity;
 
 import android.os.Bundle;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
 import hu.evehcilabs.androidbase.BaseActivity;
 import hu.evehcilabs.androidbase.TransactionSettings;
 import hu.evehcilabs.satesatesate.R;
+import hu.evehcilabs.satesatesate.databinding.ActivityMainBinding;
 import hu.evehcilabs.satesatesate.fragment.MainFragment;
 
 public class MainActivity extends BaseActivity {
 
-  protected ConstraintLayout fragmentContainer;
+  private ActivityMainBinding binding;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    initViews();
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     if (savedInstanceState == null) {
       gotoMainFragment();
     }
   }
 
-  private void initViews() {
-    fragmentContainer = findViewById(R.id.container_fragment);
-  }
-
   private void gotoMainFragment() {
     TransactionSettings settings = new TransactionSettings();
     settings.setRoot(true);
     settings.setFragment(MainFragment.newInstance());
-    settings.setTargetViewGroupId(fragmentContainer.getId());
+    settings.setTargetViewGroupId(binding.containerFragment.getId());
     addFragment(settings);
   }
 }
