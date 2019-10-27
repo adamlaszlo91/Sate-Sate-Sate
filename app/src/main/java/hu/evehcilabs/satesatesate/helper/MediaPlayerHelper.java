1d667551361e6bc715c15106b1f0d19fda499ece
Ádám László <adam.laszlo.91@gmail.com>
2019-10-27 12:55:42 +0100

    Returned to MediaPlayer from SoundPool
    fix: returned to the MediaPlayer solution for playing sounds because SoundPool could not play along one of the longer audio files.

--- app/src/main/java/hu/evehcilabs/satesatesate/helper/MediaPlayerHelper.java
+++ app/src/main/java/hu/evehcilabs/satesatesate/helper/MediaPlayerHelper.java
@@ -1,80 +1,119 @@
 package hu.evehcilabs.satesatesate.helper;
 
 import android.content.Context;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.MediaPlayer;
 import hu.evehcilabs.satesatesate.R;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MediaPlayerHelper {
-
-  public enum SoundIdentifier {
-    SATE_SATE_SATE, SATE_SATE_SATE_MULTI, TANCHOU, SATE_SATE_SATE_REMIX, TRANSPORK;
-  }
-
-  private static HashMap<SoundIdentifier, Integer> soundResourceMap =
-    new HashMap<SoundIdentifier, Integer>() {{
-      put(SoundIdentifier.SATE_SATE_SATE, R.raw.sate_sate_sate);
-      put(SoundIdentifier.SATE_SATE_SATE_MULTI, R.raw.sate_sate_sate_multi);
-      put(SoundIdentifier.TANCHOU, R.raw.ban_taisho);
-      put(SoundIdentifier.SATE_SATE_SATE_REMIX, R.raw.sate_sate_sate_remix);
-      put(SoundIdentifier.TRANSPORK, R.raw.hawk_transpork);
-    }};
-
-  private HashMap<SoundIdentifier, Integer> soundIdentifierMap = new HashMap<>();
-  private ArrayList<SoundIdentifier> loadedSounds = new ArrayList<>();
-  private Integer currentStreamId = null;
-  private SoundPool soundPool;
+  private MediaPlayer sateSateSateMediaPlayer;
+  private MediaPlayer sateSateSateMultiMediaPlayer;
+  private MediaPlayer tanchouMediaPlayer;
+  private MediaPlayer sateSateSateRemixMediaPlayer;
+  private MediaPlayer transporkMediaPlayer;
 
   public MediaPlayerHelper(Context context) {
-    soundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
-    soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
-      @Override public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
-        if (status == 0) {
-          for (Map.Entry<SoundIdentifier, Integer> entry : soundIdentifierMap.entrySet()) {
-            if (entry.getValue() == sampleId) {
-              loadedSounds.add(entry.getKey());
-              break;
-            }
-          }
-        }
-      }
-    });
-    loadSounds(context);
-  }
-
-  private void loadSounds(Context context) {
-    for (SoundIdentifier soundIdentifier : soundResourceMap.keySet()) {
-      //noinspection ConstantConditions
-      soundIdentifierMap.put(soundIdentifier,
-                             soundPool.load(context, soundResourceMap.get(soundIdentifier), 1));
-    }
-  }
-
-  private boolean isSoundLoaded(SoundIdentifier soundIdentifier) {
-    return loadedSounds.contains(soundIdentifier);
+    sateSateSateMediaPlayer = MediaPlayer.create(context, R.raw.sate_sate_sate);
+    sateSateSateMultiMediaPlayer = MediaPlayer.create(context, R.raw.sate_sate_sate_multi);
+    tanchouMediaPlayer = MediaPlayer.create(context, R.raw.ban_taisho);
+    sateSateSateRemixMediaPlayer = MediaPlayer.create(context, R.raw.sate_sate_sate_remix);
+    transporkMediaPlayer = MediaPlayer.create(context, R.raw.hawk_transpork);
   }
 
   public void destroy() {
-    stop();
-    soundPool.release();
-    soundPool = null;
+    if (sateSateSateMediaPlayer.isPlaying()) {
+      sateSateSateMediaPlayer.stop();
+    }
+    sateSateSateMediaPlayer.release();
+    if (sateSateSateMultiMediaPlayer.isPlaying()) {
+      sateSateSateMultiMediaPlayer.stop();
+    }
+    sateSateSateMultiMediaPlayer.release();
+    if (tanchouMediaPlayer.isPlaying()) {
+      tanchouMediaPlayer.stop();
+    }
+    tanchouMediaPlayer.release();
+    if (sateSateSateRemixMediaPlayer.isPlaying()) {
+      sateSateSateRemixMediaPlayer.stop();
+    }
+    sateSateSateRemixMediaPlayer.release();
+    if (transporkMediaPlayer.isPlaying()) {
+      transporkMediaPlayer.stop();
+    }
+    transporkMediaPlayer.release();
   }
 
-  public void play(SoundIdentifier soundIdentifier) {
-    if (isSoundLoaded(soundIdentifier)) {
-      //noinspection ConstantConditions
-      currentStreamId =
-        soundPool.play(soundIdentifierMap.get(soundIdentifier), 1.0f, 1.0f, 1, 0, 1.0f);
+  // region Sate sate sate
+
+  public void startSateSateSate() {
+    sateSateSateMediaPlayer.seekTo(0);
+    sateSateSateMediaPlayer.start();
+  }
+
+  public void pauseSateSateSate() {
+    if (sateSateSateMediaPlayer.isPlaying()) {
+      sateSateSateMediaPlayer.pause();
     }
   }
 
-  public void stop() {
-    if (currentStreamId != null) {
-      soundPool.stop(currentStreamId);
-      currentStreamId = null;
+  // endregion
+
+  // region Sate sate sate multi
+
+  public void startSateSateSateMulti() {
+    sateSateSateMultiMediaPlayer.seekTo(0);
+    sateSateSateMultiMediaPlayer.start();
+  }
+
+  public void pauseSateSateSateMulti() {
+    if (sateSateSateMultiMediaPlayer.isPlaying()) {
+      sateSateSateMultiMediaPlayer.pause();
     }
   }
+
+  // endregion
+
+  // region Tanchou
+
+  public void startTanchou() {
+    tanchouMediaPlayer.seekTo(0);
+    tanchouMediaPlayer.start();
+  }
+
+  public void pauseTanchou() {
+    if (tanchouMediaPlayer.isPlaying()) {
+      tanchouMediaPlayer.pause();
+    }
+  }
+
+  // endregion
+
+  // region Sate sate sate remix
+
+  public void startSateSateSateRemix() {
+    sateSateSateRemixMediaPlayer.seekTo(0);
+    sateSateSateRemixMediaPlayer.start();
+  }
+
+  public void pauseSateSateSateRemix() {
+    if (sateSateSateRemixMediaPlayer.isPlaying()) {
+      sateSateSateRemixMediaPlayer.pause();
+    }
+  }
+
+  // endregion
+
+  // region Transpork
+
+  public void startTranspork() {
+    transporkMediaPlayer.seekTo(0);
+    transporkMediaPlayer.start();
+  }
+
+  public void pauseTranspork() {
+    if (transporkMediaPlayer.isPlaying()) {
+      transporkMediaPlayer.pause();
+    }
+  }
+
+  // endregion
 }
